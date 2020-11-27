a78ad2801dfb15c993ca56017f5150b304ff9af4
vodnicearleonid <vodnicear@gmail.com>
2020-11-27 19:18:14 +0200

    Часть 2. Практика создания сайта на Laravel Управление категориями. Часть 1

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MainController;
 use Illuminate\Support\Facades\Route;
 
@@ -17,6 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
-Route::group(['prefix' => 'admin', 'namespace' => 'Admin'], function () {
+Route::group(['prefix' => 'admin'], function () {
     Route::get('/', [MainController::class, 'index'])->name('admin.index');
+    Route::resource('/categories', CategoryController::class);
 });
