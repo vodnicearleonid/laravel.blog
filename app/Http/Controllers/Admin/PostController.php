3318a96068752cc7eb46a69ba0b4c33c23e9b5d7
vodnicearleonid <vodnicear@gmail.com>
2020-12-17 22:48:05 +0200

    Часть 2. Практика создания сайта на Laravel Регистрация

--- app/Http/Controllers/Admin/PostController.php
+++ app/Http/Controllers/Admin/PostController.php
@@ -2,10 +2,10 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Category;
+use App\Models\Category;
 use App\Http\Controllers\Controller;
-use App\Post;
-use App\Tag;
+use App\Models\Tag;
+use App\Models\Post;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Storage;
 
--- routes/web.php
+++ routes/web.php
@@ -20,7 +20,8 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('home');
+
 Route::group(['prefix' => 'admin'], function () {
     Route::get('/', [MainController::class, 'index'])->name('admin.index');
     Route::resource('/categories', CategoryController::class);
